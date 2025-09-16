5861f5d03bd9227fa6f1364af06dee966565f7ff
fdmitriy <fdmitriy@inbox.ru>
2025-09-16 21:21:20 +0300

    Домашнее задание сделано, Java Collections Framework: Set. Вынесен компаратор в отдельный класс.

--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -15,37 +15,24 @@ public class ProductBasket {
         content.computeIfAbsent(newProduct.getName(), k -> new LinkedList<>()).add(newProduct);
     }
 
-//    public int getTotalCost() {
-//        int totalCost = 0;
-//        for (Map.Entry<String, List<Product>> prods : content.entrySet()) {
-//            for (final Product product : prods.getValue()) {
-//                totalCost += product.getPrice();
-//            }
-//        }
-//        return totalCost;
-//    }
-
     public int getTotalCost() {
-        return content.entrySet().stream()
-                .flatMap(p -> p.getValue().stream())
-                .mapToInt(Product::getPrice)
-                .sum();
+        int totalCost = 0;
+        for (Map.Entry<String, List<Product>> prods : content.entrySet()) {
+            for (final Product product : prods.getValue()) {
+                totalCost += product.getPrice();
+            }
+        }
+        return totalCost;
     }
 
-//    public int countNumberSpecial() {
-//        int count = 0;
-//        for (Map.Entry<String, List<Product>> prods : content.entrySet()) {
-//            for (final Product product : prods.getValue()) {
-//                if (product.isSpecial()) count += 1;
-//            }
-//        }
-//        return count;
-//    }
     public int countNumberSpecial() {
-        return  (int)content.entrySet().stream()
-                .flatMap(p -> p.getValue().stream())
-                .filter(Product::isSpecial)
-                .count();
+        int count = 0;
+        for (Map.Entry<String, List<Product>> prods : content.entrySet()) {
+            for (final Product product : prods.getValue()) {
+                if (product.isSpecial()) count += 1;
+            }
+        }
+        return count;
     }
 
     public void printContents() {
@@ -53,20 +40,14 @@ public class ProductBasket {
             System.out.println("В корзине пусто.");
             return;
         }
-        int sum = content.entrySet().stream()
-                .flatMap(linkedList -> linkedList.getValue().stream())
-                .mapToInt(product -> {
-                    System.out.println(product);
-                    return product.getPrice();
-                }).sum();
-        System.out.printf("Итого: %d%n", sum);
-//        for (Map.Entry<String, List<Product>> prods : content.entrySet()) {
-//            for (final Product product : prods.getValue()) {
-//                System.out.print(product);
-//                System.out.println();
-//            }
-//        }
-//        System.out.printf("Итого: %d%n", getTotalCost());
+        System.out.printf("Итого: %d%n", getTotalCost());
+        for (Map.Entry<String, List<Product>> prods : content.entrySet()) {
+            for (final Product product : prods.getValue()) {
+                System.out.print(product);
+                System.out.println();
+            }
+        }
+        System.out.printf("Итого: %d%n", getTotalCost());
         System.out.printf("Специальных товаров: %d%n", countNumberSpecial());
     }
 
@@ -90,18 +71,12 @@ public class ProductBasket {
     @Override
     public String toString() {
         StringBuilder printBasket = new StringBuilder("Содержание корзины:" + '\n');
-        content.entrySet().stream()
-                .flatMap(product -> {
-                    printBasket.append(product);
-                    printBasket.append('\n');
-                    return java.util.stream.Stream.empty();
-                });
-//        for (Map.Entry<String, List<Product>> prods : content.entrySet()) {
-//            for (final Product product : prods.getValue()) {
-//                printBasket.append(product);
-//                printBasket.append('\n');
-//            }
-//        }
+        for (Map.Entry<String, List<Product>> prods : content.entrySet()) {
+            for (final Product product : prods.getValue()) {
+                printBasket.append(product);
+                printBasket.append('\n');
+            }
+        }
         printBasket.deleteCharAt(printBasket.length() - 1);
         return printBasket.toString();
     }
